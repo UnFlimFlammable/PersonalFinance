c69471ee452c1fafbfcb6df6b7cdde4028126711
caylebray <braycayle@gmail.com>
2015-10-20 14:36:28 -0600

    Added an express routing

--- expressServices.js
+++ expressServices.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/getUser', function(req, res){
+
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
