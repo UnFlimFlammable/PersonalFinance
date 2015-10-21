6441e7d4fbdd43f560b42966547cf76f977f4e1a
caylebray <braycayle@gmail.com>
2015-10-21 10:24:57 -0600

    Implemented Registration and Login EWS

--- expressServices.js
+++ expressServices.js
@@ -1,13 +1,60 @@
 var express = require('express');
 var epiphany = require('./PersonalCheckingApp.js');
+var MongoClient = require('mongodb').MongoClient;
+var url = require('url');
+
+
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>');
 });
 
-app.get('/getUser', function(req, res){
+app.get('/login', function(req, res){
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
+  MongoClient.connect("mongodb://personalFinance:mugsymugsy@ds039484.mongolab.com:39484/personalfinance", function(err, db) {
+    db.createCollection('users', {strict:true}, function(err, collection) {});
+    if(err) {
+      res.send("ERROR");
+      return console.dir(err);
+    }
 
+    var collection = db.collection('users');
+    var cursor = collection.find(url_parts.query);
+
+    cursor.each(function(err, doc){
+      if(doc != null){
+        console.log(doc);
+        res.send(doc);
+      }else{
+        console.log("Error, user not found!");
+        res.send("User not found");
+      }
+    });
+  });
+});
+
+app.get('/registerUser', function(req, res){
+  MongoClient.connect("mongodb://personalFinance:mugsymugsy@ds039484.mongolab.com:39484/personalfinance", function(err, db) {
+    db.createCollection('Users', {strict:true}, function(err, collection) {}); //ignores the statement if collection already exists
+
+    if(err) {
+      res.send("ERROR");
+      return console.dir(err);
+    }
+
+    var collection = db.collection('Users');
+    var user = url.parse(req.url, true).query.user;
+
+    collection.insert(user);
+    var cursor = db.Users.find(user);
+    if(cursor.hasNext()){
+      res.send(db.Users.find(user).next());
+    }
+
+
+  });
 });
 
 var server = app.listen(3000, function () {
